3e68491af449dd68436e7d7732d09a85f61dbbc4
Shivam Raghav <shivamraghav00000@gmail.com>
2024-09-30 15:57:08 +0530

    added port

--- api/index.js
+++ api/index.js
@@ -7,13 +7,14 @@ import productRouter from "./routes/product.js";
 import cartRouter from "./routes/cart.js";
 import orderRouter from "./routes/order.js";
 import paymentRouter from "./routes/payment.js";
-import cors from "cors" ;
+import cors from "cors";
 const app = express();
 app.use(express.json());
 dotenv.config();
 // Apply CORS middleware
-app.use(cors()); 
+app.use(cors());
 
+const PORT = process.env.PORT;
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -31,8 +32,6 @@ app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 app.use("/api", paymentRouter);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
-
