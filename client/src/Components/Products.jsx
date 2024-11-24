fbfee14083a234e343e662169b8011229b8a7c1b
Shivam Raghav <shivamraghav00000@gmail.com>
2024-11-25 03:39:32 +0530

    added Pagination to products page

--- client/src/Components/Products.jsx
+++ client/src/Components/Products.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { useEffect, useMemo, useState } from "react";
 import styled from "styled-components";
-import popularProducts from "../data";
 import Product from "./Product";
 import axios from "axios";
-import useFetchProducts from "./useFetchProducts";
+import NavigateNextOutlinedIcon from "@mui/icons-material/NavigateNextOutlined";
+import NavigateBeforeOutlinedIcon from "@mui/icons-material/NavigateBeforeOutlined";
 
 const Container = styled.div`
   display: flex;
@@ -17,9 +17,27 @@ const ProductWrapper = styled.div`
   min-width: 250px;
   box-sizing: border-box;
 `;
+
+const Pagination = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 10px;
+  button {
+    margin: 0 10px;
+    padding: 5px 10px;
+    cursor: pointer;
+  }
+`;
+
+const Button = styled.button`
+  border: none;
+  background-color: transparent;
+`;
 const Products = ({ cat, filters, sort }) => {
   const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 8;
 
   useEffect(() => {
     const getProducts = async () => {
@@ -60,20 +78,51 @@ const Products = ({ cat, filters, sort }) => {
     }
   }, [filteredProducts, sort]);
 
+  // paginate the products
+
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = products.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
+
+  const totalPages = Math.ceil(products.length / productsPerPage);
+
   return (
-    <Container>
-      {cat
-        ? filteredProducts.map((item) => (
-            <ProductWrapper key={item._id}>
-              <Product item={item} />
-            </ProductWrapper>
-          ))
-        : products.slice(0, 8).map((item) => (
-            <ProductWrapper key={item._id}>
-              <Product item={item} />
-            </ProductWrapper>
-          ))}
-    </Container>
+    <>
+      <Container>
+        {cat
+          ? filteredProducts.map((item) => (
+              <ProductWrapper key={item._id}>
+                <Product item={item} />
+              </ProductWrapper>
+            ))
+          : currentProducts.slice(0, productsPerPage).map((item) => (
+              <ProductWrapper key={item._id}>
+                <Product item={item} />
+              </ProductWrapper>
+            ))}
+      </Container>
+      {/* Pagination Controls */}
+      <Pagination>
+        <Button
+          onClick={() => setCurrentPage(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          <NavigateBeforeOutlinedIcon />
+        </Button>
+        <span style={{ display: "flex", alignItems: "center" }}>
+          {currentPage} / {totalPages}
+        </span>
+        <Button
+          onClick={() => setCurrentPage(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        >
+          <NavigateNextOutlinedIcon />
+        </Button>
+      </Pagination>
+    </>
   );
 };
 
